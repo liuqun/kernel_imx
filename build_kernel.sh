f67e4b7172f2ec64a1a10bcb7850aea881eb48c6
cym <caoym@topeet.com>
2016-07-06 19:11:33 -0700

    initial commit

---
+++ build_kernel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export ARCH=arm
+export CROSS_COMPILE=/media/work/iTOP-iMAX6/iTOP-iMX6_android4.4.2/myandroid/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
+cp  ./arch/arm/configs/topeet_imx6_android_supper_defconfig  .config
+make zImage -j4
+
---
+++ make_clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export ARCH=arm
+export CROSS_COMPILE=/media/work/iTOP-iMAX6/iTOP-iMX6_android4.4.2/myandroid/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
+make distclean
+
