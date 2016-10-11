cb824f06b6cad0b7908bcd41996fe5de98081610
cym <caoym@topeet.com>
2016-10-10 20:36:35 -0700

    modify build kernel script

--- build_qt_kernel.sh
+++ build_qt_kernel.sh
@@ -2,7 +2,9 @@
 
 export ARCH=arm
 export CROSS_COMPILE=$(pwd)/../prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
-cp arch/arm/configs/topeet_imx6_linux_supper_defconfig .config
+
+cp config_for_iTOPiMX6_linux .config
+
 make zImage -j4
 
 make uImage -j4
